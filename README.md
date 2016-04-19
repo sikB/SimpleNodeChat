9e8f8c6b73f09a5d100f092fcf6a91217077f721
sikB <sikhovski@icloud.com>
2016-04-19 17:03:22 -0400

    Node chat room
    simple chat room
    working on css
    working on time stamp
    working on saved messages

---
+++ README.md
@@ -0,0 +1,4 @@
+#Node.JS chatroom Exercise with the addition of Heroku
+
+###Created a basic chat room and hosting that other people can join
+###Added time stamps to the chatroom and user names
---
+++ chat.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(req,res){
+	fs.readFile('index.html', 'utf-8', function(error, data){
+		res.writeHead(200, {'content-type': 'text/html'});
+		res.write(data);
+		res.end();
+	});
+});
+
+var io = require('socket.io').listen(server);
+messageArray = [];
+numUsers = 0;
+
+io.sockets.on('connect', function(socket){
+	numUsers++;
+	io.sockets.emit('message_to_client',{
+		numUsers: numUsers
+	});
+
+	socket.on('message_to_server', function(data){
+		var date = new Date();
+		io.sockets.emit('message_to_client', {
+			message: data.message,
+			 date: date
+			});
+		messageArray.push(data.message);
+	});
+});
+
+server.listen(8000);
+console.log('Local host is running');
