b4160d70f511a06ae26f6a7e29b2534dcedf0387
sikB <sikhovski@icloud.com>
2016-04-19 17:13:57 -0400

    testing heroku

--- chat.js
+++ chat.js
@@ -1,8 +1,10 @@
 var http = require('http');
 var fs = require('fs');
+var port = process.env.PORT || '8000';
 var server = http.createServer(function(req,res){
 	fs.readFile('index.html', 'utf-8', function(error, data){
 		res.writeHead(200, {'content-type': 'text/html'});
+		res.write('<script>var port=' +port+'</script>');
 		res.write(data);
 		res.end();
 	});
@@ -28,5 +30,5 @@ io.sockets.on('connect', function(socket){
 	});
 });
 
-server.listen(8000);
+server.listen(port);
 console.log('Local host is running');
